8cc541030f88b06cf79d3c6c188464bfe4a3a89a
yacshy <962215611@qq.com>
2025-05-14 18:47:34 +0800

    ...

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Sudoku from "@/components/Sudoku.vue";
+</script>
+
+<template>
+  <Sudoku />
+</template>
+
+<style scoped lang="css"></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import TDesign from 'tdesign-vue-next';
+import 'tdesign-vue-next/es/style/index.css';
+
+createApp(App).use(TDesign).mount('#app')

0a36360e1e4944ada66a6679e3dc4cefcad01fb3
yacshy <962215611@qq.com>
2025-05-14 23:08:07 +0800

    ...

--- src/views/Sudoku.vue
+++ src/views/Sudoku.vue
@@ -7,17 +7,14 @@
             gridTemplateRows: `repeat(${gridTemplate.row}, 1fr)`
         }"
     >
-        <template
-            v-for="(item, index) in imgs"
+        <div
+            v-for="(item, index) in imgsSorted"
             :key="index"
-        >
-            <div
-                class="img"
-                :style="{
-                    background: `url(${item}) no-repeat center / cover`
-                }"
-            ></div>
-        </template>
+            class="img"
+            :style="{
+                background: `url(${item}) no-repeat center / cover`
+            }"
+        ></div>
     </div>
 
     <div class="w-full h-[50px] items-center flex gap-x-[20px]">
@@ -40,23 +37,35 @@
                 :min="-2"
             />
         </div>
+        <div class="flex items-center gap-x-[5px]">
+            <Input v-model="gridTemplate.sort" />
+            <Button @click="onEnter">resort</Button>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { Button, InputNumber } from 'tdesign-vue-next'
+import { Button, InputNumber, Input } from 'tdesign-vue-next'
 import Html2Img from 'html2img'
 import { reactive, ref } from 'vue'
+import clonedeep from 'lodash.clonedeep'
 
 const gridTemplate = reactive({
     col: 1,
-    row: 1
+    row: 1,
+    sort: ''
 })
 
 const containRef = ref()
 // const pickInpRef = ref()
 
 const imgs = ref([])
+
+const imgsSorted = ref([])
+const onEnter = () => {
+    const arr = gridTemplate.sort.split(',').map(i => parseInt(i))
+	imgsSorted.value = arr.map((index) => imgs.value[index])
+}
 
 const pickImg = async () => {
     imgs.value = []
@@ -67,14 +76,14 @@ const pickImg = async () => {
     inp.multiple = true
 
     inp.onchange = async () => {
-        const files = inp.files
+        const files = Array.from(inp.files)
 
-        console.log(files)
+        gridTemplate.sort = files.map((_, index) => index).join(',')
 
-        for (const file of files) {
-            const url = await readAsUrl(file)
-            imgs.value.push(url)
-        }
+        const urls = await Promise.all(files.map((file) => readAsUrl(file)))
+
+        imgs.value = urls
+        imgsSorted.value = imgs.value
     }
 
     inp.click()

351f131ef926e47af47ed6e7248634b2847c1401
yacshy <962215611@qq.com>
2025-05-14 19:23:12 +0800

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+    plugins: [vue()],
+    build: {
+        outDir: 'docs'
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     }
-  }
 })
